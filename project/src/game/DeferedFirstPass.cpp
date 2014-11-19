3ee52035aa207bab6b53d391ebaf758fd35788ef
Thomas Coeffic <thomas.coeffic@grenoble-inp.org>
2014-11-19 11:54:50 +0100

    Added Camera control to DeferedFirstPass

--- project/src/game/DeferedFirstPass.cpp
+++ project/src/game/DeferedFirstPass.cpp
@@ -19,23 +19,20 @@ DeferedFirstPass::DeferedFirstPass():
     ground("obj/ground.obj",ShaderProgram::loadFromFile(
         "shader/geometryPass.vert",
         "shader/geometryPass.frag"
-    ))
+    )),
+	camera(NULL)
 {
+	camera = new Camera();
     glCheckError(__FILE__,__LINE__);
 }
 
 
 void DeferedFirstPass::firstPass()
 {
+	camera->update();
+	view = camera->lookAt(glm::vec3(0.0,3.0,0.0));
 
-    static float t=0;
-    t+=0.02;
     projection = glm::perspective(70.0f, 640.0f/480.0f, 0.1f, 100.0f);
-    view = glm::lookAt(
-            glm::vec3(10.0*sin(t),5.0,10.0*cos(t)),
-            glm::vec3(0.0,3,0.0),
-            glm::vec3(0.0,1.0,0.0)
-    );
     obj.getShader().use();
     obj.getShader().setUniform("projection",projection);
     obj.getShader().setUniform("view",view);
